f1297a5cbf5abe18f7961e719d6eb69ef42d3113
Thalys Wolf <thalyswolf01@gmail.com>
2024-01-21 10:25:26 -0300

    routes

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -6,6 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
   },
+  {
+    path: '/chat',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/ChatPage.vue') }]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
